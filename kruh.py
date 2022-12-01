a04c7eda13424d30ecdf7651d7016201916cf296
Kankys <lukas.kanka@lukan.cz>
2022-12-01 12:16:01 +0000

    Přidat „kruh.py“

---
+++ kruh.py
@@ -0,0 +1,6 @@
+print("Zadej poloměr kruhu pro výpočet obvodu a obsahu:")
+a = float(input())
+obvod = 2 * 3.1415 * a
+obsah = 3.1415 * (a * a)
+print(f"Obvod je: {obvod} cm")
+print(f"Obsah je: {obsah} cm^2")
