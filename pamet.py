5b9ff85e8e95817f494fe447571a69d382cf6186
Kankys <lukas.kanka@lukan.cz>
2022-12-01 12:16:49 +0000

    Přidat „pamet.py“

---
+++ pamet.py
@@ -0,0 +1,12 @@
+s1 = "Tohle je nějaký text"
+s2 = s1
+print(s1)
+print(s2)
+
+print("Další možnost zapsání")
+
+s1 = "Tohle je nějaký text"
+s2 = s1
+s2 = "Tohle je nějaký další text"
+print(s1)
+print(s2)
