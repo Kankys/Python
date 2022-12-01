7f7cb3978c6bae0d0123191fe6747400d78b0dd8
Kankys <lukas.kanka@lukan.cz>
2022-12-01 12:28:40 +0000

    Přidat „formatovaci_operatory.py“

---
+++ formatovaci_operatory.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+c = a + b
+d = "Když sečteme %d a %d, dostaneme %d" % (a, b, c)
+print(d)
+# Formátování je velice užitečné pro tvoření komplexních řetězců z většího množství proměnných. 
+# Zde jsou nejzákladnější zástupné znaky pro nejčastější proměnné:
+
+# %d - Celá čásla (int)
+# %s - Řetězce(str)
+# %f - Reálná čásla (float) 
